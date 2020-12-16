29301c1d5765df6c1d22fb0aeb6c9eff70cc2bdb
mrmangohands <mrmangohands@protonmail.com>
2020-12-16 11:02:22 -0800

    change: Backport to Minecraft 1.16.1
    This change is originally from PhiPro95/mc-fixes@e92ff03. Technically
    this is still being built against 1.16.3, as remapping would only add
    friction when rebasing onto upstream at the moment. Nonetheless, I've
    opted to depend on 1.16.1 and name the artifacts as such rather than
    make the mixin optional or add some complexity to the loading system,
    partially because I'd expect supporting the same versions as upstream
    to confuse end users.
    
    These decisions do seem a bit silly together, but hopefully this clears
    things up :P

---
+++ src/main/java/me/jellysquid/mods/phosphor/mixin/client/network/MixinClientPlayNetworkHandler.java
@@ -0,0 +1,24 @@
+package me.jellysquid.mods.phosphor.mixin.client.network;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.chunk.light.LightingProvider;
+
+@Mixin(ClientPlayNetworkHandler.class)
+public abstract class MixinClientPlayNetworkHandler
+{
+    @Redirect(
+        method = "onChunkData(Lnet/minecraft/network/packet/s2c/play/ChunkDataS2CPacket;)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/chunk/light/LightingProvider;setColumnEnabled(Lnet/minecraft/util/math/ChunkPos;Z)V"
+        )
+    )
+    private void cancelDisableLightUpdates(final LightingProvider lightingProvider, final ChunkPos pos, final boolean enable)
+    {
+    }
+}
